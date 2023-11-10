06030dca2815b28b8fa4acf7bd8b04b09477c35b
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2023-11-10 15:10:07 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//<div class="parent">
-//  <div class="child">
-//    <h1>h1</h1>
-//    <h2>h2</h2>
-//  </div>
-//</div>;
 
-// const heading = React.createElement("h1", { id: "heading" }, "hellow world");
+/* 
+  const heading = React.createElement("h1", { id: "heading" }, "hellow world"); 
+*/
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "hellow"),
-    React.createElement("h2", { id: "heading" }, "world"),
-  ])
+/* 
+  const heading = (
+    <div class="parent">
+      <div class="child">
+        <h1 id="heading">hellow</h1>
+        <h2 id="heading">world</h2>
+      </div>
+    </div>
+  ); 
+*/
+
+const Heading = () => (
+  <div class="parent">
+    <div class="child">
+      <h1 id="heading">hellow</h1>
+      <h2 id="heading">world</h2>
+    </div>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(<Heading />);
